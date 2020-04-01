07b1c5e7065213b193546d0c25610f5edf84d622
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2020-04-01 17:55:31 +0100

    Add README. Display some rows from first few columns of interest in Countries table.

--- CoronavirusAnalyser/Program.fs
+++ CoronavirusAnalyser/Program.fs
@@ -1,8 +1,20 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open FSharp.Data
 
-open System
+[<Literal>]
+let url = @"https://www.worldometers.info/coronavirus/#countries"
+
+type ConfirmedCases = HtmlProvider<url>
+
+let explore() =
+    let data = ConfirmedCases.GetSample()
+    let tables = data.Tables
+    tables.Main_table_countries_today.Rows
+    |> Array.take 20
+    |> Array.map (fun row -> row.``Country, Other``, row.``Total Cases``, row.``New Cases``, row.``Total Deaths``, row.``New Deaths``)
+    |> Array.iter (fun (country, totalCases, newCases, totalDeaths, newDeaths) -> 
+        printfn "Country: %s, Total Cases: %M, New Cases: %.0f, Total Deaths: %.0f, New Deaths: %.0f" country totalCases newCases totalDeaths newDeaths)
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    explore()
     0 // return an integer exit code
---
+++ README.md
@@ -0,0 +1,1 @@
+# Worldometer Coronavirus Analysis 
