05863a691b8a3250bc036c16cc575e3e86095705
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2020-04-02 18:33:09 +0100

    Output header and just values for rows.

--- CoronavirusAnalyser/Program.fs
+++ CoronavirusAnalyser/Program.fs
@@ -9,11 +9,18 @@ type ConfirmedCases = HtmlProvider<url>
 let explore() =
     let data = ConfirmedCases.GetSample()
     let tables = data.Tables
-    tables.Main_table_countries_today.Rows
+
+    let headers = tables.Main_table_countries_today.Headers 
+    match headers with 
+    | Some x -> printfn "%s, %s, %s, %s, %s" x.[0] x.[1] x.[2] x.[3] x.[4]
+    | None -> printfn ""     
+
+    let rows =tables.Main_table_countries_today.Rows
+    rows
     |> Array.take 20
     |> Array.map (fun row -> row.``Country, Other``, row.``Total Cases``, row.``New Cases``, row.``Total Deaths``, row.``New Deaths``)
     |> Array.iter (fun (country, totalCases, newCases, totalDeaths, newDeaths) -> 
-        printfn "Country: %s, Total Cases: %M, New Cases: %.0f, Total Deaths: %.0f, New Deaths: %.0f" country totalCases (if Double.IsNaN(newCases) then 0.0 else newCases) totalDeaths (if Double.IsNaN(newDeaths) then 0.0 else newDeaths))
+        printfn "%s, %M, %.0f, %.0f, %.0f" country totalCases (if Double.IsNaN(newCases) then 0.0 else newCases) totalDeaths (if Double.IsNaN(newDeaths) then 0.0 else newDeaths))
 
 [<EntryPoint>]
 let main argv =
