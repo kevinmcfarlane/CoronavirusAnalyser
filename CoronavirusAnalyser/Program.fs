158d30827089bc343a0e459f980d47c4e2e92798
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2020-04-08 12:57:27 +0100

    Add new columns and new calculated columns.

--- CoronavirusAnalyser/Program.fs
+++ CoronavirusAnalyser/Program.fs
@@ -14,20 +14,22 @@ let printToConsole() =
 
     // Headers
     match tables.Main_table_countries_today.Headers with 
-    | Some headers -> printfn "%s, %s, %s, %s, %s" (headers.[0].Replace(",", " or")) headers.[1] headers.[2] headers.[3] headers.[4]
+    | Some headers -> printfn "%s, %s, %s, %s, %s, %s, %s" (headers.[0].Replace(",", " or")) headers.[1] headers.[2] headers.[3] headers.[4] headers.[5] (headers.[7].Replace(",", " or"))
     | None -> ()     
 
     // Rows
     tables.Main_table_countries_today.Rows
     |> Array.take 21
-    |> Array.map (fun row -> row.``Country, Other``, row.``Total Cases``, row.``New Cases``, row.``Total Deaths``, row.``New Deaths``)
-    |> Array.iter (fun (country, totalCases, newCases, totalDeaths, newDeaths) -> 
-        printfn "%s, %M, %.0f, %.0f, %.0f" 
+    |> Array.map (fun row -> row.``Country, Other``, row.``Total Cases``, row.``New Cases``, row.``Total Deaths``, row.``New Deaths``, row.``Total Recovered``, row.``Serious, Critical``)
+    |> Array.iter (fun (country, totalCases, newCases, totalDeaths, newDeaths, totalRecovered, seriousCritical) -> 
+        printfn "%s, %M, %.0f, %.0f, %.0f, %.0f, %.0f" 
             country 
             totalCases 
             (if Double.IsNaN(newCases) then 0.0 else newCases) 
             totalDeaths 
-            (if Double.IsNaN(newDeaths) then 0.0 else newDeaths))
+            (if Double.IsNaN(newDeaths) then 0.0 else newDeaths)
+            totalRecovered
+            seriousCritical)
 
 let printToCsv() =
     
@@ -41,24 +43,29 @@ let printToCsv() =
     // (tables.Main_table_countries_today.Headers is string [] option, not string [] so we must pattern-match)
     let headers =
         match tables.Main_table_countries_today.Headers with 
-        | Some h -> sprintf "%s, %s, %s, %s, %s" (h.[0].Replace(",", " or")) h.[1] h.[2] h.[3] h.[4]
+        | Some h -> sprintf "%s, %s, %s, %s, %s, %s, %s" (h.[0].Replace(",", " or")) h.[1] h.[2] h.[3] h.[4] h.[5] (h.[7].Replace(",", " or"))
         | None -> ""    
 
-    let extendedHeaders = headers + ", Total Deaths / Total Cases"
+    let extendedHeaders = headers + ", Total Deaths / Total Cases, Total Recovered / Total Cases, Serious or Critical / Total Cases"
     stream.WriteLine(extendedHeaders)
 
     // Rows
     tables.Main_table_countries_today.Rows
     |> Array.take 21
-    |> Array.map (fun row -> row.``Country, Other``, row.``Total Cases``, row.``New Cases``, row.``Total Deaths``, row.``New Deaths``)
-    |> Array.iter (fun (country, totalCases, newCases, totalDeaths, newDeaths) -> 
-        stream.WriteLine(sprintf "%s, %M, %.0f, %.0f, %.0f, %.3f" 
+    |> Array.map (fun row -> row.``Country, Other``, row.``Total Cases``, row.``New Cases``, row.``Total Deaths``, row.``New Deaths``, row.``Total Recovered``, row.``Serious, Critical``)
+    |> Array.iter (fun (country, totalCases, newCases, totalDeaths, newDeaths, totalRecovered, seriousCritical) -> 
+        stream.WriteLine(sprintf "%s, %M, %.0f, %.0f, %.0f, %.0f, %.0f, %.3f, %.3f, %.3f" 
             country 
             totalCases 
             (if Double.IsNaN(newCases) then 0.0 else newCases) 
             totalDeaths 
             (if Double.IsNaN(newDeaths) then 0.0 else newDeaths) 
-            (totalDeaths / float totalCases)))
+            totalRecovered
+            seriousCritical
+            (totalDeaths / float totalCases)
+            (totalRecovered / float totalCases)
+            (seriousCritical / float totalCases)
+            ))
 
 [<EntryPoint>]
 let main argv =
