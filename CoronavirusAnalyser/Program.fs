5be4423444f4891f29e3d15aa927549c1fb7da78
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2020-04-15 12:17:37 +0100

    Remove printToConsole. Output all rows. Add local helper. Remove all NaN output. Update README.

--- CoronavirusAnalyser/Program.fs
+++ CoronavirusAnalyser/Program.fs
@@ -10,42 +10,22 @@ let PercentConverter = 100.0
 
 type ConfirmedCases = HtmlProvider<url>
 
-let printToConsole() =
-    
-    let data = ConfirmedCases.GetSample()
-    let tables = data.Tables
-
-    // Headers
-    match tables.Main_table_countries_today.Headers with 
-    | Some headers -> printfn "%s, %s, %s, %s, %s, %s, %s" (headers.[0].Replace(",", " or")) headers.[1] headers.[2] headers.[3] headers.[4] headers.[5] (headers.[7].Replace(",", " or"))
-    | None -> ()     
-
-    // Rows
-    tables.Main_table_countries_today.Rows
-    |> Array.take 21
-    |> Array.map (fun row -> row.``Country, Other``, row.``Total Cases``, row.``New Cases``, row.``Total Deaths``, row.``New Deaths``, row.``Total Recovered``, row.``Serious, Critical``)
-    |> Array.iter (fun (country, totalCases, newCases, totalDeaths, newDeaths, totalRecovered, seriousCritical) -> 
-        printfn "%s, %M, %.0f, %.0f, %.0f, %.0f, %.0f" 
-            country 
-            totalCases 
-            (if Double.IsNaN(newCases) then 0.0 else newCases) 
-            totalDeaths 
-            (if Double.IsNaN(newDeaths) then 0.0 else newDeaths)
-            totalRecovered
-            seriousCritical)
-
 let printToCsv() =
+
+    let getDouble v =
+        (if Double.IsNaN(v) then 0.0 else v) 
     
     let outputPath = Path.Combine(AppContext.BaseDirectory, "output.csv")
     use stream = new StreamWriter(outputPath)
 
     let data = ConfirmedCases.GetSample()
-    let tables = data.Tables
+    let table = data.Tables.Main_table_countries_today
+    let rowCount = table.Rows.Length
 
     // Headers
-    // (tables.Main_table_countries_today.Headers is string [] option, not string [] so we must pattern-match)
+    // (table.Headers is string [] option, not string [] so we must pattern-match)
     let headers =
-        match tables.Main_table_countries_today.Headers with 
+        match table.Headers with 
         | Some h -> sprintf "%s, %s, %s, %s, %s, %s, %s" (h.[0].Replace(",", " or")) h.[1] h.[2] h.[3] h.[4] h.[5] (h.[7].Replace(",", " or"))
         | None -> ""    
 
@@ -53,25 +33,24 @@ let printToCsv() =
     stream.WriteLine(extendedHeaders)
 
     // Rows
-    tables.Main_table_countries_today.Rows
-    |> Array.take 21
+    table.Rows
+    |> Array.take rowCount
     |> Array.map (fun row -> row.``Country, Other``, row.``Total Cases``, row.``New Cases``, row.``Total Deaths``, row.``New Deaths``, row.``Total Recovered``, row.``Serious, Critical``)
     |> Array.iter (fun (country, totalCases, newCases, totalDeaths, newDeaths, totalRecovered, seriousCritical) -> 
         stream.WriteLine(sprintf "%s, %M, %.0f, %.0f, %.0f, %.0f, %.0f, %.2f%%, %.2f%%, %.2f%%" 
             country 
             totalCases 
-            (if Double.IsNaN(newCases) then 0.0 else newCases) 
-            totalDeaths 
-            (if Double.IsNaN(newDeaths) then 0.0 else newDeaths) 
-            totalRecovered
-            seriousCritical
-            (PercentConverter * (totalDeaths / float totalCases))
-            (PercentConverter * (totalRecovered / float totalCases))
-            (PercentConverter * (seriousCritical / float totalCases))
+            (getDouble newCases) 
+            (getDouble totalDeaths) 
+            (getDouble newDeaths) 
+            (getDouble totalRecovered) 
+            (getDouble seriousCritical) 
+            (PercentConverter * ((getDouble totalDeaths) / float totalCases))
+            (PercentConverter * ((getDouble totalRecovered) / float totalCases))
+            (PercentConverter * ((getDouble seriousCritical) / float totalCases))
             ))
 
 [<EntryPoint>]
 let main argv =
     printToCsv()
-    printToConsole()
     0 
--- README.md
+++ README.md
@@ -3,53 +3,7 @@
 Worldometer [Coronavirus] daily statistics. Analysis of the [Countries] table.
 
 
-#### Results - initial version
 
-Just display some rows from the first few columns of interest. (E.g.,  April 02, 2020 around 12 GMT.)
-
-
-Country: USA, Total Cases: 215357, New Cases: 354, Total Deaths: 5113, New Deaths: 11
-
-Country: Italy, Total Cases: 110574, New Cases: 0, Total Deaths: 13155, New Deaths: 0
-
-Country: Spain, Total Cases: 110238, New Cases: 6120, Total Deaths: 10003, New Deaths: 616
-
-Country: Germany, Total Cases: 78983, New Cases: 1002, Total Deaths: 948, New Deaths: 17
-
-Country: France, Total Cases: 56989, New Cases: 0, Total Deaths: 4032, New Deaths: 0
-
-Country: Iran, Total Cases: 50468, New Cases: 2875, Total Deaths: 3160, New Deaths: 124
-
-Country: UK, Total Cases: 29474, New Cases: 0, Total Deaths: 2352, New Deaths: 0
-
-....
-
-....
-
-#### Results - next iteration
-
-CSV output of headers and rows.
-
-
-Country - Other, Total Cases, New Cases, Total Deaths, New Deaths
-
-USA, 236072, 21069, 5777, 675
-
-Italy, 115242, 4668, 13915, 760
-
-Spain, 110238, 6120, 10096, 709
-
-Germany, 83459, 5478, 1048, 117
-
-France, 56989, 0, 4032, 0
-
-Iran, 50468, 2875, 3160, 124
-
-UK, 33718, 4244, 2921, 569
-
-....
-
-....
 
 #### Results - Excel output for Top 20 countries by Total Cases with extra calculated columns
 
