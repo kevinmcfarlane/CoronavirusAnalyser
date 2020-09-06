4a24897b556ed3721956df5cb6961c19f06859f8
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2020-09-06 18:06:26 +0100

    Shift header indices across by 1. Replace header index magic numbers with named constants. Insert 'Active Cases' column between 'Total Recovered' and 'Serious, Critical.'

--- CoronavirusAnalyser/Program.fs
+++ CoronavirusAnalyser/Program.fs
@@ -10,6 +10,24 @@ let PercentConverter = 100.0
 
 type ConfirmedCases = HtmlProvider<url>
 
+// Header Indices
+[<Literal>]
+let CountryOrOther = 1
+[<Literal>]
+let TotalCases = 2
+[<Literal>]
+let NewCases = 3
+[<Literal>]
+let TotalDeaths = 4
+[<Literal>]
+let NewDeaths = 5
+[<Literal>]
+let TotalRecovered = 6
+[<Literal>]
+let ActiveCases = 8
+[<Literal>]
+let SeriousOrCritical = 9
+
 let printToCsv() =
 
     let getDouble v =
@@ -26,7 +44,7 @@ let printToCsv() =
     // (table.Headers is string [] option, not string [] so we must pattern-match)
     let headers =
         match table.Headers with 
-        | Some h -> sprintf "%s, %s, %s, %s, %s, %s, %s" (h.[0].Replace(",", " or")) h.[1] h.[2] h.[3] h.[4] h.[5] (h.[7].Replace(",", " or"))
+        | Some h -> sprintf "%s, %s, %s, %s, %s, %s, %s, %s" (h.[CountryOrOther].Replace(",", " or")) h.[TotalCases] h.[NewCases] h.[TotalDeaths] h.[NewDeaths] h.[TotalRecovered] h.[ActiveCases] (h.[SeriousOrCritical].Replace(",", " or"))
         | None -> ""    
 
     let extendedHeaders = headers + ", Total Deaths / Total Cases, Total Recovered / Total Cases, Serious or Critical / Total Cases"
@@ -35,15 +53,16 @@ let printToCsv() =
     // Rows
     table.Rows
     |> Array.take rowCount
-    |> Array.map (fun row -> row.``Country, Other``, row.``Total Cases``, row.``New Cases``, row.``Total Deaths``, row.``New Deaths``, row.``Total Recovered``, row.``Serious, Critical``)
-    |> Array.iter (fun (country, totalCases, newCases, totalDeaths, newDeaths, totalRecovered, seriousCritical) -> 
-        stream.WriteLine(sprintf "%s, %M, %.0f, %.0f, %.0f, %.0f, %.0f, %.2f%%, %.2f%%, %.2f%%" 
+    |> Array.map (fun row -> row.``Country, Other``, row.``Total Cases``, row.``New Cases``, row.``Total Deaths``, row.``New Deaths``, row.``Total Recovered``, row.``Active Cases``, row.``Serious, Critical``)
+    |> Array.iter (fun (country, totalCases, newCases, totalDeaths, newDeaths, totalRecovered, activeCases, seriousCritical) -> 
+        stream.WriteLine(sprintf "%s, %M, %.0f, %.0f, %.0f, %.0f, %.0f, %.0f, %.2f%%, %.2f%%, %.2f%%" 
             country 
             totalCases 
             (getDouble newCases) 
             (getDouble totalDeaths) 
             (getDouble newDeaths) 
             (getDouble totalRecovered) 
+            (getDouble activeCases) 
             (getDouble seriousCritical) 
             (PercentConverter * ((getDouble totalDeaths) / float totalCases))
             (PercentConverter * ((getDouble totalRecovered) / float totalCases))
